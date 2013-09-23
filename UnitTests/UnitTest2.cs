5121fecce5228810a71b87779f3fdae54eb44b57
DevinLevy <analescheok@hotmail.com>
2013-09-22 22:50:12 -0600

    hw week4
    homework for week four

--- UnitTests/UnitTest2.cs
+++ UnitTests/UnitTest2.cs
@@ -1,80 +1,86 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WeekThreeProject;
-using System.Linq;
+using WebApplication3;
+using System.Collections.Generic;
 using System.Data;
-using System.Data.Objects;
-using System.Data.Entity;
-using System.Data.Services.Client;
+using System.Linq;
 
 namespace UnitTests
 {
-    /// <summary>
-    /// Summary description for UnitTest2
-    /// </summary>
     [TestClass]
     public class UnitTest2
     {
-        public UnitTest2()
+        [TestMethod]
+        public void InsertPictureUsingRepository()
         {
-            //
-            // TODO: Add constructor logic here
-            //
+            var pictureRepo = new DataRepository<Picture>();
+            Picture pic = new Picture();
+            pic.PersonId = 1;
+            pic.Id = 1;
+            pictureRepo.Insert(pic);
         }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
+        
+       
+        [TestMethod]
+        public void RetreiveLabUsingRepository()
         {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
+            var labRepo = new DataRepository<Lab>();
+            List<Lab> list = labRepo.GetAll().ToList<Lab>();
+            Assert.IsTrue(list.Count > 0);
+            
         }
+    [TestMethod]
+        public void RetreiveSchedUsingRepository()
+    {
+        var schedRepo = new DataRepository<Schedule>();
+        List<Schedule>   list = schedRepo.GetAll().ToList<Schedule>();
+        Assert.IsTrue(list.Count > 0);
+    }
+    [TestMethod]
+    public void RetreivePersonUsingRepository()
+    {
+        var personRepo = new DataRepository<Person>();
+        List<Person> list = personRepo.GetAll().ToList<Person>();
+        Assert.IsTrue(list.Count > 0);
+    }
 
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
+
 
         [TestMethod]
-        public void TestMethod1()
+        public void GetALabByKey()
         {
-             int value = 1;
-             using (var context = new Model1Container())
-             {
-                 Person p = new Person();
-                 p.FirstName = "Tom";
-                 p.LastName = "Brocaw";
-                 context.People.Add(p);
-                 context.SaveChanges();
-             }
+            var labRepo = new DataRepository<Lab>();
+           // List<Lab> list = labRepo.GetBySpecificKey("ID", 1);
 
         }
+        [TestMethod]
+        public void UpdateRepo()
+        {
+            var personRepo = new DataRepository<Person>();
+            List<Person> myList = personRepo.GetAll().ToList<Person>();
+            myList[1].FirstName = "Marsella";
+            personRepo.Update(myList[1]);
+
+            myList = personRepo.GetAll().ToList<Person>();
+            Assert.AreEqual(myList[1].FirstName, "Marsella");
+        }
+        [TestMethod]
+        public void UpdateRepoLab()
+        {
+            var labRepo = new DataRepository<Lab>();
+            List<Lab> myList = labRepo.GetAll().ToList<Lab>();
+            myList[2].ScheduleId = 5;
+            labRepo.Update(myList[2]);
+
+            myList = labRepo.GetAll().ToList<Lab>();
+            Assert.AreEqual(myList[2].ScheduleId, 5);
+        }
+        
+
+
+        
+        
+           
+        
     }
 }
--- WeekThreeProject/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeekThreeProject.Global" Language="C#" %>
